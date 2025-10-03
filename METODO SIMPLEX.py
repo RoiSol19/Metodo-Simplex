fbb91ee00595de928d1825cd7735b3af7a124c9d
martinezgaby66304 <martinezgaby66304@gmail.com>
2025-10-02 19:05:28 -0600

    Mostrar tabla ,importaciones

--- METODO SIMPLEX.py
+++ METODO SIMPLEX.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 boton_color = "#AEDFF7"
 hover_color = "#9FD4F0"
 texto_color = "#2B6CB0"
@@ -14,3 +17,7 @@ def gauss_jordan(tableau, pivot_row, pivot_col):
         if i != pivot_row:
             tableau[i] -= tableau[i, pivot_col] * tableau[pivot_row]
     return tableau
+def mostrar_tabla(tabla, pivot_row=None, pivot_col=None, resultado=None):
+    plt.figure(figsize=(5, 3))
+    ax = sns.heatmap(tabla, annot=True, cmap="PuBuGn", cbar=False,
+                     linewidths=0.5, linecolor='white', fmt=".2f")
